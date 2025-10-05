04a5e387075be54f5c59be39a0a21367ec8f8532
Pramodsnz01 <pramodsnz01@gmail.com>
2025-10-05 10:22:17 +0545

    adding github link to source code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,9 +17,12 @@ const Navbar = () => {
                         <a className='hover:font-bold' href="#">Contact</a>
                     </li>
                 </ul> */}
-                <button className='text-white bg-green-800 flex rounded-full my-5 justify-between items-center cursor-pointer ring-white ring-1'>
+                <button 
+                    onClick={() => window.open('https://github.com/Pramodsnz01/Password-Manager', '_blank')}
+                    className='text-white bg-green-800 flex rounded-full my-5 justify-between items-center cursor-pointer ring-white ring-1 hover:bg-green-700 transition-colors'
+                >
                     <img className='invert p-1.5 w-10' src="/icons/github.svg" alt="github logo" />
-                    <span className='font-bold px-2'>GitHub</span>
+                    <span className='font-bold px-2'>View Source</span>
                 </button>
             </div>
         </nav>
