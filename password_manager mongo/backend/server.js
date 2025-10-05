f00b2f1de3dffebface126772784ac01f068f9f5
Pramodsnz01 <pramodsnz01@gmail.com>
2025-10-05 09:00:11 +0545

    creating apis

--- password_manager mongo/backend/server.js
+++ password_manager mongo/backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const { MongoClient } = require('mongodb');
+const bodyparser = require('body-parser')
 
 dotenv.config()
 
@@ -12,15 +13,35 @@ const client = new MongoClient(url);
 const dbName = 'password-manager';
 const app = express()
 const port = 3000
+app.use(bodyparser.json())
 
 client.connect();
 console.log('Connected successfully to server');
 
+//Get all passwords
 app.get('/', async (req, res) => {
     const db = client.db(dbName);
-    const collection = db.collection('documents');
+    const collection = db.collection('passwords');
     const findResult = await collection.find({}).toArray();
     res.json(findResult)
+})
+
+//Save a password
+app.post('/', async (req, res) => {
+    const password = req.body
+    const db = client.db(dbName);
+    const collection = db.collection('passwords');
+    const findResult = await collection.insertOne(password);
+    res.send({success: true, result: findResult})
+})
+
+//Delete a password by id
+app.delete('/', async (req, res) => {
+    const password = req.body
+    const db = client.db(dbName);
+    const collection = db.collection('passwords');
+    const findResult = await collection.deleteOne(password);
+    res.send({success: true, result: findResult})
 })
 
 app.listen(port, () => {
